c40251bffcf6266e97a4a293ef18ba63a92cba08
Amitay Horwitz <amitayh@gmail.com>
2017-12-17 07:53:23 +0200

    Initial commit

---
+++ src/main/java/katas/bowling/Game.java
@@ -0,0 +1,4 @@
+package katas.bowling;
+
+public class Game {
+}
---
+++ src/test/java/katas/bowling/GameTest.java
@@ -0,0 +1,14 @@
+package katas.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void testAllGutter() {
+        assertEquals(1, 1);
+    }
+
+}

fe4490ece493a924c94fcfcd0e85e8157fd2cb48
Amitay Horwitz <amitayh@gmail.com>
2017-12-18 13:46:06 +0200

    Implemented bowling game score calculator + tests

---
+++ src/main/java/katas/bowling/Game.java
@@ -0,0 +1,51 @@
+package katas.bowling;
+
+class Game {
+
+    private int[] rolls = new int[20];
+
+    private int rollIndex = 0;
+
+    public void roll(int pins) {
+        rolls[rollIndex++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += frameScore(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int frameScore(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+}
--- src/test/java/katas/bowling/GameTest.java
+++ src/test/java/katas/bowling/GameTest.java
@@ -1,14 +1,64 @@
 package katas.bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
-    void test() {
-        assertEquals(1, 1);
+    void testGutterGame() {
+        rollMany(20, 0);
+
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    void testAllOnes() {
+        rollMany(20, 1);
+
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    void testOneSpare() {
+        game.roll(5);
+        game.roll(5); // Spare
+        game.roll(3);
+        rollMany(17, 0);
+
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    void testOneStrike() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
+        rollMany(16, 0);
+
+        assertEquals(24, game.score());
+    }
+
+    @Test
+    void testPerfectGame() {
+        rollMany(12, 10);
+
+        assertEquals(300, game.score());
+    }
+
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 
 }
